99ed5998427026a3bc3ab457b47b6f260e111328
Arttu Olli <60509537+ArttuOll@users.noreply.github.com>
2026-01-07 19:20:13 +0200

    Init with Cobra

---
+++ cmd/root.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gosh",
+	Short: "A simple shell written in Go",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+}
