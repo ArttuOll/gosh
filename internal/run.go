bf525fb5d9e2554a4949826ae102ac175ff50a37
Arttu Olli <60509537+ArttuOll@users.noreply.github.com>
2026-01-07 19:50:28 +0200

    Add ability to execute commands

--- internal/run.go
+++ internal/run.go
@@ -3,27 +3,38 @@ package internal
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-func Run(cmd *cobra.Command, args []string) error {
+func Run(cmd *cobra.Command, args []string) {
 	stdInReader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Printf("gosh> ")
 		input, err := stdInReader.ReadString('\n')
 		if err != nil {
-			return fmt.Errorf("failed to read from stdin: %w", err)
+			log.Fatal("failed to read from stdin: %w", err)
 		}
 
 		parts := strings.Fields(input)
 		command := parts[0]
+		arguments := parts[1:]
 
 		if command == "exit" {
 			os.Exit(0)
+		}
+
+		cmd := exec.Command(command, arguments...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			fmt.Println(err)
 		}
 	}
 }
